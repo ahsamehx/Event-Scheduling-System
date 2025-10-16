29ea62cb92d1cac56348af93879f3a2d5fe4ff23
Ahmed Sameh <ahmedmusic0909@gmail.com>
2025-10-16 20:50:39 +0300

    initial commit

--- app.js
+++ app.js
@@ -3,29 +3,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import  {PORT} from './config/env.js';
-import router from './routes/routes.js';
-import connectToDatabase from './Database/mongodb.js';
-import errorMiddleware from './Middlewares/error.js';
+
 import cookieParser from 'cookie-parser';
-import arcjetMiddleware from './Middlewares/arcjet.js';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(arcjetMiddleware);
+
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Subscription Tracker API!');
+  res.send('Welcome to the Event Scheduling System API!');
 });
 
-app.use('/api/v1', router);
-
-app.use(errorMiddleware)
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  await connectToDatabase();
 });
 
 export default app;
