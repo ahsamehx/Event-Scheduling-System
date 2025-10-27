2358943abf52003391130677ddcda8542bb6d214
HusamAbozide <husam.shahein@gmail.com>
2025-10-27 20:12:45 +0300

    Update init.sql

--- Database/init.sql
+++ Database/init.sql
@@ -1,10 +1,12 @@
 
 create table if not exists users (
     user_id serial primary key,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
     username varchar(50) unique not null,
     password_hash varchar(255) not null,
     email varchar(100) unique not null,
-    created_at timestamp default current_timestamp
+    created_at timestamp default CURRENT_TIMESTAMP
 );
 
 
